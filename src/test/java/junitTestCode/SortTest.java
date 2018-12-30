b8000b2244f1833c11c5bfecab314d565756fdb2
SIS <SIS@SIS-PC>
2018-12-30 20:57:24 +0900

    이진 검색트리 추가

--- src/test/java/junitTestCode/SortTest.java
+++ src/test/java/junitTestCode/SortTest.java
@@ -11,8 +11,8 @@ import org.junit.Test;
 public class SortTest {
 	int[] numbers = { 4, 7, 1, 6, 3, 5, 4 };
 	int[] afterSorting = { 1, 3, 4, 4, 5, 6, 7 };
-	List<Integer> numbersList = Arrays.asList(4, 7, 1, 6, 3, 5, 4);
-	List<Integer> sortingList = Arrays.asList(1, 3, 4, 4, 5, 6, 7);
+	public static List<Integer> numbersList = Arrays.asList(4, 7, 1, 6, 3, 5, 4);
+	public static List<Integer> sortingList = Arrays.asList(1, 3, 4, 4, 5, 6, 7);
 	private Sort sort = new Sort();
 
 //	@Test
@@ -23,11 +23,16 @@ public class SortTest {
 			System.out.println(numbers[i]);
 		}
 
-		assertArrayEquals(numbers, afterSorting);
+		assertArrayEquals(afterSorting, numbers);
 	}
 
-	@Test
+//	@Test
 	public void insertSortTest() {
-		assertEquals(sort.insertSort(numbersList), sortingList);
+		assertEquals(sortingList, sort.insertSort(numbersList));
+	}
+	
+	@Test
+	public void quickSortTest() {
+		assertEquals(sortingList, sort.quickSort(numbersList));
 	}
 }
